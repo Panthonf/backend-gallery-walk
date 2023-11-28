26fdc24f90c043a01beeeb40082f8afa0242c7f4
Panthon <panthon_kansap@cmu.ac.th>
2023-11-28 21:36:57 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+import Fastify from "fastify";
+const PORT = process.env.PORT || 5000;
+const fastify = Fastify({
+  logger: true,
+});
+
+import { items } from "./Items.js";
+
+// Declare a route
+fastify.get("/", (request, reply) => {
+  reply.send(items);
+});
+
+fastify.get("/:id", (request, reply) => {
+  const { id } = request.params;
+  const item = items.find((item) => item.id === Number(id));
+  reply.send(item);
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen({ port: PORT });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
