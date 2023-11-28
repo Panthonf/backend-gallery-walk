37cb20233b39717c1c84c16566b9d1e399d603b0
Panthon <panthon_kansap@cmu.ac.th>
2023-11-28 23:05:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
 import Fastify from "fastify";
-const PORT = process.env.PORT || 5000;
 const fastify = Fastify({
   logger: true,
 });
 
-import { items } from "./Items.js";
+fastify.register(import("./routes/items.js"));
 
-// Declare a route
-fastify.get("/items", (request, reply) => {
-  reply.send(items);
-});
-
-fastify.get("/:id", (request, reply) => {
-  const { id } = request.params;
-  const item = items.find((item) => item.id === Number(id));
-  reply.send(item);
-});
-
-// Run the server!
+const PORT = process.env.PORT || 3000;
 const start = async () => {
   try {
-    await fastify.listen({ port: PORT });
+    await fastify.listen({ port: PORT, host: "0.0.0.0" });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
