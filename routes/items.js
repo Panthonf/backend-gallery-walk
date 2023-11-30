af2da6357d7bc0cb396681a0074720c1f1b4c8c1
Panthon <panthon_kansap@cmu.ac.th>
2023-11-30 09:53:29 +0700

    Update items.js

--- routes/items.js
+++ routes/items.js
@@ -1,10 +1,16 @@
-import { getItems, getItem } from "../controllers/itemsController.js";
+import {
+  getItems,
+  getItem,
+  addItem,
+  deleteItem,
+  updateItem,
+} from "../controllers/itemsController.js";
 
 // Item schema
 const Item = {
   type: "object",
   properties: {
-    id: { type: "number" },
+    id: { type: "string" },
     name: { type: "string" },
     price: { type: "number" },
   },
@@ -32,9 +38,60 @@ const getItemOpts = {
   handler: getItem,
 };
 
+const postItemOpts = {
+  schema: {
+    body: {
+      type: "object",
+      required: ["name", "price"],
+      properties: {
+        name: { type: "string" },
+        price: { type: "number" },
+      },
+    },
+    response: {
+      201: Item,
+    },
+  },
+  handler: addItem,
+};
+
+const deleteItemOpts = {
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          message: { type: "string" },
+          success: { type: "boolean" },
+        },
+      },
+    },
+  },
+  handler: deleteItem,
+};
+
+const updateItemOpts = {
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          message: { type: "string" },
+          success: { type: "boolean" },
+          data: Item,
+        },
+      },
+    },
+  },
+  handler: updateItem,
+};
+
 function itemRoutes(fastify, options, done) {
   fastify.get("/items", getItemsOpts);
   fastify.get("/items/:id", getItemOpts);
+  fastify.post("/items", postItemOpts);
+  fastify.delete("/items/:id", deleteItemOpts);
+  fastify.put("/items/:id", updateItemOpts);
   done();
 }
 
