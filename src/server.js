6388f3be4aa24df786e6b682144c7b2f933c566e
Panthonf <panthon_kansap@cmu.ac.th>
2023-12-03 23:29:59 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
-import server from './app.js';
+import server from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 
-server.get('/', async (request, reply) => {
-    return { hello: 'world' };
+server.get("/", async (request, reply) => {
+  return { hello: "world" };
 });
 
 const start = async () => {
-    try {
-        await server.listen({ port: PORT });
-        console.log(`Server listening on port http://localhost:${server.server.address().port}`);
-    } catch (err) {
-        server.log.error(err);
-        process.exit(1);
-    }
+  try {
+    await server.listen({ port: PORT, host: "0.0.0.0" });
+    console.log(
+      `Server listening on port http://localhost:${
+        server.server.address().port
+      }`
+    );
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
 };
 
 start();
