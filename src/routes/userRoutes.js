820bd0c056e86d047b2abefc4973c80bdd61889a
Panthonf <panthon_kansap@cmu.ac.th>
2023-12-04 00:33:20 +0700

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import userController from '../controllers/userController.js'
 
 export default async (fastify) => {
-  fastify.get('/users', userController.getAllUsers);
+  fastify.get('/users',{ schema: { tags: ['API', 'User'] } }, userController.getAllUsers);
   fastify.get('/users/:id', userController.getUserById);
   fastify.post('/users', userController.createUser);
   fastify.put('/users/:id', userController.updateUser);
