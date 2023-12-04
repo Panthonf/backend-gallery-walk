1079a7b99e8396f425a2b41316817165b71a47b7
Panthonf <panthon_kansap@cmu.ac.th>
2023-12-04 23:28:24 +0700

    add: test auth
    - validate route, path
    - login, logout
    - user info from google auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,5 @@
 import oauthPlugin from "@fastify/oauth2";
+import axios from "axios";
 
 const CALLBACK_URI =
   process.env.CALLBACK_URI || "http://localhost:3000/login/google/callback";
@@ -39,5 +40,46 @@ export default async (fastify) => {
     // const { token: newToken } = await this.getNewAccessTokenUsingRefreshToken(token)
 
     reply.send({ access_token: token.access_token });
+  });
+
+  const isLoggedIn = async (request, reply) => {
+    const { token } = request.cookies;
+
+    if (!token) {
+      reply.code(401).send({ message: "You are not logged in" });
+      return false;
+    }
+
+    return true;
+  };
+
+  fastify.decorate("isLoggedIn", isLoggedIn);
+
+  fastify.get(
+    "/user",
+    { preValidation: [fastify.isLoggedIn] },
+    async (request, reply) => {
+      try {
+        const { token } = request.cookies;
+        const { data } = await axios.get(
+          "https://www.googleapis.com/oauth2/v3/userinfo",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        const { name, email, picture } = data;
+        reply.send({ name, email, picture });
+      } catch (error) {
+        console.error("Error fetching user info:", error);
+        reply.code(500).send({ message: "Internal Server Error" });
+      }
+    }
+  );
+
+  fastify.get("/logout", async (request, reply) => {
+    reply.clearCookie("token").send({ message: "You are logged out" });
   });
 };
