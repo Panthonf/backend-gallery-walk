8ababc4725c8ad3322c077a99a3a99185d3b9be2
Panthonf <panthon_kansap@cmu.ac.th>
2023-12-16 16:35:48 +0700

    Update routes.js

--- src/features/users/routes.js
+++ src/features/users/routes.js
@@ -10,13 +10,6 @@ export default async (fastify) => {
   fastify.get("/", getAllUsersController);
   fastify.get("/profile", getUserByIdController);
   fastify.post("/", createUserController);
-  // fastify.put("/:id", userController.updateUser);
   fastify.delete("/:id", deleteUserController);
-  fastify.get("/test", async (req, res) => {
-    return {
-      test: "test",
-    };
-  });
-
   fastify.get("/:email", getUserByEmailService)
 };
