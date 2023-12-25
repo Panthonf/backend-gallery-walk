b5d2e1ef766273f5ffdb52760df503365b10fd13
Panthonf <panthon_kansap@cmu.ac.th>
2023-12-25 13:12:18 +0700

    Create models.js

---
+++ src/features/guests/models.js
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function getEventByEventId(eventId) {
+  const event = await prisma.events.findUnique({
+    where: {
+      id: eventId,
+    },
+  });
+  return event;
+}
+
+async function getAllEvents() {
+  const events = await prisma.events.findMany();
+  return events;
+}
+
+
+export {
+    getEventByEventId,
+    getAllEvents
+}
