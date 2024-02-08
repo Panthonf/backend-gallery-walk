c90226fbf79ef08a0f3aac19788d2fc9ea55b5b7
Panthonf <panthon_kansap@cmu.ac.th>
2024-02-08 23:59:14 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ dotenv.config();
 const server = Fastify({ logger: true });
 
 server.register(import("@fastify/cors"), {
-  origin: "*",
+  origin: process.env.FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
@@ -35,29 +35,29 @@ server.register(secureSession, {
   cookieName: "Set-Cookie", // This should be the name of the cookie header
 });
 
-// await server.register(import("@fastify/swagger"));
-// await server.register(import("@fastify/swagger-ui"), {
-//   routePrefix: "/docs",
-//   uiConfig: {
-//     docExpansion: "full",
-//     deepLinking: false,
-//   },
-//   uiHooks: {
-//     onRequest: function (request, reply, next) {
-//       next();
-//     },
-//     preHandler: function (request, reply, next) {
-//       next();
-//     },
-//   },
-//   staticCSP: true,
-//   transformStaticCSP: (header) => header,
-//   transformSpecification: (swaggerObject, request, reply) => {
-//     return swaggerObject;
-//   },
-//   transformSpecificationClone: true,
-//   exposeRoute: true,
-// });
+await server.register(import("@fastify/swagger"));
+await server.register(import("@fastify/swagger-ui"), {
+  routePrefix: "/docs",
+  uiConfig: {
+    docExpansion: "full",
+    deepLinking: false,
+  },
+  uiHooks: {
+    onRequest: function (request, reply, next) {
+      next();
+    },
+    preHandler: function (request, reply, next) {
+      next();
+    },
+  },
+  staticCSP: true,
+  transformStaticCSP: (header) => header,
+  transformSpecification: (swaggerObject, request, reply) => {
+    return swaggerObject;
+  },
+  transformSpecificationClone: true,
+  exposeRoute: true,
+});
 
 // Include your routes
 server.register(import("./features/users/routes.js"), { prefix: "/users" });
