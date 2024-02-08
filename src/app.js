258434a26fa38343fdcec0c2a4877992d34fc210
Panthonf <panthon_kansap@cmu.ac.th>
2024-02-08 21:29:17 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ dotenv.config();
 const server = Fastify({ logger: true });
 
 server.register(import("@fastify/cors"), {
-  origin: "http://localhost:5173", // Replace with your frontend URL
+  origin: process.env.FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
   // allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
