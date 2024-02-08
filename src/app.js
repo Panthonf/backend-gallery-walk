a8be9ec91e942d7ad98ce2d547ac0da83aac05fc
Panthonf <panthon_kansap@cmu.ac.th>
2024-02-09 02:06:34 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,14 +3,11 @@ import dotenv from "dotenv";
 import { isLoggedIn } from "./middleware/isLoggedIn.js";
 import { isGuestLoggedIn } from "./middleware/isGuestLoggedIn.js";
 import { checkSessionMiddleware } from "./middleware/checkSessionMiddleware.js";
-
 dotenv.config();
 const server = Fastify({ logger: true });
-
-server.register(import('@fastify/cors'), {
+server.register(import("@fastify/cors"), {
   origin: ["https://frontend-gallery-walk.vercel.app", "http://localhost:3000"], // Specify allowed origins
   methods: ["GET", "POST", "PUT", "DELETE"],
-  // allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 });
 const secureSession = import("@fastify/secure-session");
@@ -21,43 +18,17 @@ server.register(secureSession, {
     httpOnly: true,
     secure: true, // Change to true if using HTTPS
     sameSite: "lax",
-    cookieName: "Set-Cookie",
   },
   saveUninitialized: false,
   resave: false,
+  cookieName: "Set-Cookie",
 });
 
 server.register(import("@fastify/multipart"));
-
 // server.register(import("@fastify/cookie"));
 server.decorate("isLoggedIn", isLoggedIn);
 server.decorate("checkSessionMiddleware", checkSessionMiddleware);
 server.decorate("isGuestLoggedIn", isGuestLoggedIn);
-
-
-await server.register(import("@fastify/swagger"));
-await server.register(import("@fastify/swagger-ui"), {
-  routePrefix: "/docs",
-  uiConfig: {
-    docExpansion: "full",
-    deepLinking: false,
-  },
-  uiHooks: {
-    onRequest: function (request, reply, next) {
-      next();
-    },
-    preHandler: function (request, reply, next) {
-      next();
-    },
-  },
-  staticCSP: true,
-  transformStaticCSP: (header) => header,
-  transformSpecification: (swaggerObject, request, reply) => {
-    return swaggerObject;
-  },
-  transformSpecificationClone: true,
-  exposeRoute: true,
-});
 
 // Include your routes
 server.register(import("./features/users/routes.js"), { prefix: "/users" });
