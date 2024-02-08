faee94949a51e1c9e6c8a97fa4c40c12e3a27354
Panthonf <panthon_kansap@cmu.ac.th>
2024-02-09 02:40:59 +0700

    Update routes.js

--- src/features/users/routes.js
+++ src/features/users/routes.js
@@ -10,4 +10,20 @@ export default async (fastify) => {
   fastify.get("/profile", getUserByIdController);
   fastify.get("/:userId", getUserByUserIdService);
   fastify.delete("/:userId", deleteUserController);
+  fastify.get("get-session", async (request, reply) => {
+    const userId = await request.session.get("user");
+    if (await request.session.get("user")) {
+      reply.send({
+        success: true,
+        message: "User logged in successfully",
+        data: request.session.get("user"),
+      });
+    } else {
+      reply.send({
+        success: false,
+        message: "User not logged in",
+        data: userId || null,
+      });
+    }
+  });
 };
