54f222b77034ba08bbbe80fff1f10fbcd7efce20
Panthonf <panthon_kansap@cmu.ac.th>
2024-02-08 21:44:58 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const server = Fastify({ logger: true });
 server.register(import("@fastify/cors"), {
   origin: process.env.FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
-  // allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 });
 
