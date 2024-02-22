f95cc01ae1856666a0e7eb49cc5675b006b7daad
Panthonf <panthon_kansap@cmu.ac.th>
2024-02-22 21:15:38 +0700

    Update routes.js

--- src/features/events/routes.js
+++ src/features/events/routes.js
@@ -9,7 +9,8 @@ import {
   getEventManagerInfoService,
   checkEventRoleService,
   updateEventService,
-  getEventFeedbackService
+  getEventFeedbackService,
+  getEventResultService
 } from "./services.js";
 
 const schema = {
@@ -89,4 +90,6 @@ export default async (fastify) => {
   fastify.get("/role/:eventId", checkEventRoleService);
 
   fastify.get("/event-feedback/:eventId", getEventFeedbackService);
+
+  fastify.get("/event-result/:eventId", getEventResultService);
 };
